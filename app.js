ec0cf794c54e523231b46aaed91af9b8b6314a89
Tulika Chaudharie <tulikac@microsoft.com>
2020-09-28 14:03:12 +0000

    Added a new route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var highcpu = require('./routes/highcpu');
 
 var app = express();
 
@@ -26,10 +27,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/highcpu', highcpu);
 
 app.use('/api', (req, res, next) => {
     res.json({ "result": "success" });
 });
+
+//app.use('/highcpu', highcpu);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/highcpu.js
@@ -0,0 +1,18 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    let result = 0;
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 100; j++) {
+            for (let k = 0; k < 10; k++) {
+                result = i + j + k + result;
+            }
+        }
+    }
+    res.send('Hopefully taking some CPU cycles');
+});
+
+module.exports = router;
